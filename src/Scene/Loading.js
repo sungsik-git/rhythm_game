83f16cd56bbce0088aef8dcf7562f7fcc1f8fbf8
sungsik-git <kss950708@gamil.com>
2024-01-07 23:14:09 +0900

    Feat: game 선택화면 구현

--- src/Scene/Loading.js
+++ src/Scene/Loading.js
@@ -7,28 +7,33 @@ export default class Loading extends Phaser.Scene {
   }
 
   preload() {
+    //background
     this.load.image('bg',bg);
-
-    //loading progress
-    this.load.on('progress', (value) => {
-      console.log(value);
-    })
-
-    this.load.on('complete', () => {
-        console.log('complete')
-    })
   }
 
   create() {
-    const bgImg = this.add.image(0,0,'bg').setOrigin(0);
-    bgImg.displayWidth = this.game.config.width
-    bgImg.displayHeight = this.game.config.height
+    const bgImg = this.add.image(0, 0, 'bg').setOrigin(0);
+    bgImg.displayWidth = this.game.config.width;
+    bgImg.displayHeight = this.game.config.height;
 
-    this.add.text(100, 200, "Loading...", {fontSize: '32px', fill: '#fff'})
+    //start button 
+    this.clickToStart = this.add.text(
+      this.game.config.width / 2,
+      this.game.config.height * 3 / 4,
+      'Click to Start'
+    )
+    .setFill("#fff")
+    .setFontSize(50)
+    .setOrigin(0.5)
+    .setDepth(999)
+    .setAlign('center')
+    .setInteractive();
 
-    this.time.delayedCall(2000, ()=> {
-      this.scene.start('main');
+    this.input.once('pointerdown', ()=> {
+      console.log("click Start")
+      this.scene.transition({target:'main', duration:"500"})
     });
+
   }
     
   update() {
--- src/Scene/Main.js
+++ src/Scene/Main.js
@@ -1,4 +1,9 @@
 import Phaser from "phaser";
+import game1_thumbnail from "../img/game1_thumbnail.jpg";
+import game2_thumbnail from "../img/game2_thumbnail.jpg";
+import game3_thumbnail from "../img/game3_thumbnail.jpg";
+import game4_thumbnail from "../img/game4_thumbnail.jpg";
+import game5_thumbnail from "../img/game5_thumbnail.jpg";
 
 export default class Main extends Phaser.Scene {
   constructor() {
@@ -6,39 +11,58 @@ export default class Main extends Phaser.Scene {
   }
 
   preload() {
-    this.load.baseURL = 'https://yungjoong.github.io/';
-    this.load.image('logo',  'angular.png');
-  
-    for (let i=0;  i<500; i++)  {
-      this.load.image('logo'+i,  'angular.png');
-    }
-    let progressBar = this.add.graphics();
-    let progressBox = this.add.graphics();
-
-    this.load.on('progress', (value) => {
-  
-        progressBar.clear();
-        progressBar.fillStyle(0x00ff00, 1);
-        progressBar.fillRoundedRect(250, 460, 300 * value, 30, 5);
-      });
-  
-
-    progressBar.clear();
-    progressBar.fillStyle(0x00ff00, 1);
-    progressBox.fillRoundedRect(240,450,320,50,5);
-
-    this.load.on('complete', () => {
-
-        progressBar.destroy();
-        progressBox.destroy();
-    })
-}
+    this.load.image('game1_thumbnail', game1_thumbnail);
+    this.load.image('game2_thumbnail', game2_thumbnail);
+    this.load.image('game3_thumbnail', game3_thumbnail);
+    this.load.image('game4_thumbnail', game4_thumbnail);
+    this.load.image('game5_thumbnail', game5_thumbnail);
+  }
 
   create() {
-    const logo = this.add.image(400,  300,  'logo');
+    const thumbnails = [
+      'game1_thumbnail',
+      'game2_thumbnail',
+      'game3_thumbnail',
+      'game4_thumbnail',
+      'game5_thumbnail',
+    ];
+
+    let currentIndex = 0;
+
+    const currentThumbnail = this.add.image(0, 0, thumbnails[currentIndex]).setOrigin(0);
+    currentThumbnail.displayWidth = this.game.config.width;
+    currentThumbnail.displayHeight = this.game.config.height;
+
+    const nextButton = this.add.text(700, 500, 'Next', { fill: '#fff' }).setInteractive();
+    const prevButton = this.add.text(100, 500, 'Prev', { fill: '#fff' }).setInteractive();
+
+    const nextThumbnail = () => {
+      currentIndex = (currentIndex + 1) % thumbnails.length;
+      this.slideThumbnail(currentThumbnail, thumbnails, currentIndex, -this.game.config.width);
+    };
+
+    const prevThumbnail = () => {
+      currentIndex = (currentIndex - 1 + thumbnails.length) % thumbnails.length;
+      this.slideThumbnail(currentThumbnail, thumbnails, currentIndex, this.game.config.width);
+    };
+
+    nextButton.on('pointerdown', nextThumbnail);
+    prevButton.on('pointerdown', prevThumbnail);
+  }
+
+  slideThumbnail(target, thumbnails, index, targetX) {
+    this.tweens.add({
+      targets: target,
+      x: targetX,
+      duration: 500,
+      onComplete: () => {
+        target.setTexture(thumbnails[index]);
+        target.x = 0;
+      },
+    });
   }
 
   update() {
-
+    // 게임 업데이트 로직
   }
 }
