7540c2170e690e93c1b509e282b1e5f661b6b4f3
sungsik-git <kss950708@gamil.com>
2024-01-09 12:58:44 +0900

    Feat: Key info 추가

--- src/Scene/GameUI.js
+++ src/Scene/GameUI.js
@@ -1,6 +1,9 @@
 import Phaser from "phaser";
 
+
+
 export default class GameUI extends Phaser.Scene {
+    
     constructor() {
         super('gameUI');
     }
@@ -10,34 +13,44 @@ export default class GameUI extends Phaser.Scene {
     }
 
     create() {
+        const ratioWidth = this.game.config.width;
+        const ratioHeight = this.game.config.height;
+
         //node가 떨어질 route
-        const nodeRouteS = this.add.rectangle(this.game.config.width / 9 + this.calcRoute(1, 0), this.game.config.height / 2, 100, 600, 0x8662f0);
-        const nodeRouteD = this.add.rectangle(this.game.config.width / 9 + this.calcRoute(2, 1), this.game.config.height / 2, 100, 600, 0x8662f0);
-        const nodeRouteF = this.add.rectangle(this.game.config.width / 9 + this.calcRoute(3, 2), this.game.config.height / 2, 100, 600, 0x8662f0);
-        const nodeRouteSpace = this.add.rectangle(this.game.config.width / 9 + this.calcRoute(4, 3), this.game.config.height / 2, 100, 600, 0x8662f0);
-        const nodeRouteJ = this.add.rectangle(this.game.config.width / 9 + this.calcRoute(5, 4), this.game.config.height / 2, 100, 600, 0x8662f0);
-        const nodeRouteK = this.add.rectangle(this.game.config.width / 9 + this.calcRoute(6, 5), this.game.config.height / 2, 100, 600, 0x8662f0);
-        const nodeRouteL = this.add.rectangle(this.game.config.width / 9 + this.calcRoute(7, 6), this.game.config.height / 2, 100, 600, 0x8662f0);
+        const nodeRouteS = this.add.rectangle(ratioWidth / 9 + this.calcRoute(1, 0), ratioHeight / 2, 100, 600, 0x8662f0);
+        const nodeRouteD = this.add.rectangle(ratioWidth / 9 + this.calcRoute(2, 1), ratioHeight / 2, 100, 600, 0x8662f0);
+        const nodeRouteF = this.add.rectangle(ratioWidth / 9 + this.calcRoute(3, 2), ratioHeight / 2, 100, 600, 0x8662f0);
+        const nodeRouteSpace = this.add.rectangle(ratioWidth / 9 + this.calcRoute(4, 3), ratioHeight / 2, 100, 600, 0x8662f0);
+        const nodeRouteJ = this.add.rectangle(ratioWidth / 9 + this.calcRoute(5, 4), ratioHeight / 2, 100, 600, 0x8662f0);
+        const nodeRouteK = this.add.rectangle(ratioWidth / 9 + this.calcRoute(6, 5), ratioHeight / 2, 100, 600, 0x8662f0);
+        const nodeRouteL = this.add.rectangle(ratioWidth / 9 + this.calcRoute(7, 6), ratioHeight / 2, 100, 600, 0x8662f0);
 
         //route 경계선
-        const nodeRouteLine1 = this.add.rectangle(this.game.config.width / 9 - 0.5, this.game.config.height / 2, 2, 600, 0x000000);
-        const nodeRouteLine2 = this.add.rectangle(this.game.config.width / 9 + this.calcRouteLine(1, 1), this.game.config.height / 2, 2, 600, 0x000000);
-        const nodeRouteLine3 = this.add.rectangle(this.game.config.width / 9 + this.calcRouteLine(2, 2), this.game.config.height / 2, 2, 600, 0x000000);
-        const nodeRouteLine4 = this.add.rectangle(this.game.config.width / 9 + this.calcRouteLine(3, 3), this.game.config.height / 2, 2, 600, 0x000000);
-        const nodeRouteLine5 = this.add.rectangle(this.game.config.width / 9 + this.calcRouteLine(4, 4), this.game.config.height / 2, 2, 600, 0x000000);
-        const nodeRouteLine6 = this.add.rectangle(this.game.config.width / 9 + this.calcRouteLine(5, 5), this.game.config.height / 2, 2, 600, 0x000000);
-        const nodeRouteLine7 = this.add.rectangle(this.game.config.width / 9 + this.calcRouteLine(6, 6), this.game.config.height / 2, 2, 600, 0x000000);
-        const nodeRouteLine8 = this.add.rectangle(this.game.config.width / 9 + this.calcRouteLine(7, 7), this.game.config.height / 2, 2, 600, 0x000000);
+        const nodeRouteLine1 = this.add.rectangle(ratioWidth / 9 - 0.5, ratioHeight / 2, 2, 600, 0x000000);
+        const nodeRouteLine2 = this.add.rectangle(ratioWidth / 9 + this.calcRouteLine(1, 1), ratioHeight / 2, 2, 600, 0x000000);
+        const nodeRouteLine3 = this.add.rectangle(ratioWidth / 9 + this.calcRouteLine(2, 2), ratioHeight / 2, 2, 600, 0x000000);
+        const nodeRouteLine4 = this.add.rectangle(ratioWidth / 9 + this.calcRouteLine(3, 3), ratioHeight / 2, 2, 600, 0x000000);
+        const nodeRouteLine5 = this.add.rectangle(ratioWidth / 9 + this.calcRouteLine(4, 4), ratioHeight / 2, 2, 600, 0x000000);
+        const nodeRouteLine6 = this.add.rectangle(ratioWidth / 9 + this.calcRouteLine(5, 5), ratioHeight / 2, 2, 600, 0x000000);
+        const nodeRouteLine7 = this.add.rectangle(ratioWidth / 9 + this.calcRouteLine(6, 6), ratioHeight / 2, 2, 600, 0x000000);
+        const nodeRouteLine8 = this.add.rectangle(ratioWidth / 9 + this.calcRouteLine(7, 7), ratioHeight / 2, 2, 600, 0x000000);
 
         //judgement Line
         const judgementLine = this.add.graphics();
         judgementLine.lineStyle(2, 0x000000);
         judgementLine.beginPath();
-        judgementLine.moveTo(this.game.config.width / 9 ,this.game.config.height * 4 / 5);
-        judgementLine.lineTo(this.game.config.width * 9 / 10 - 2 , this.game.config.height * 4 / 5);
+        judgementLine.moveTo(ratioWidth / 9 ,ratioHeight * 4 / 5);
+        judgementLine.lineTo(ratioWidth * 9 / 10 - 2 , ratioHeight * 4 / 5);
         judgementLine.strokePath();
 
-
+        //Key Info
+        const keyS = this.add.text(ratioWidth * 2 / 12 - 5, ratioHeight * 5 / 6, 'S');
+        const keyD = this.add.text(ratioWidth * 2 / 12 - 5 + 100, ratioHeight * 5 / 6, 'D');
+        const keyF = this.add.text(ratioWidth * 2 / 12 - 5 + 200, ratioHeight * 5 / 6, 'F');
+        const keySpace = this.add.text(ratioWidth * 2 / 12 - 5 + 285, ratioHeight * 5 / 6, 'Space');
+        const keyJ = this.add.text(ratioWidth * 2 / 12 - 5 + 405, ratioHeight * 5 / 6, 'J');
+        const keyK = this.add.text(ratioWidth * 2 / 12 - 5 + 505, ratioHeight * 5 / 6, 'K');
+        const keyL = this.add.text(ratioWidth * 2 / 12 - 5 + 605, ratioHeight * 5 / 6, 'L');
     }
     
     
