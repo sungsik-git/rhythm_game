999968d083a98d2ebfd6da245fb305d78a90385a
sungsik-git <kss950708@gamil.com>
2024-01-12 10:07:49 +0900

    Feat: node와 nodes 클래스 틀 구현

---
+++ src/scenes/node/Node.js
@@ -0,0 +1,10 @@
+class Node{
+    static nodeNumber = 1;
+
+    constructor(startTime, key, pressTime){
+        this.nodeNumber = Node.nodeNumber++;
+        this.startTime = startTime;
+        this.key = key;
+        this.pressTime = pressTime;
+    }   
+}
---
+++ src/scenes/node/Nodes.js
@@ -0,0 +1,7 @@
+class Nodes{
+    constructor(){
+        this.nodes = [];
+    }
+
+    
+}
