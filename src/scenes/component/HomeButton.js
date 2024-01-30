dd02b09be5984c2c2379f502c5d4665ac6cea958
sungsik-git <kss950708@gamil.com>
2024-01-30 10:13:20 +0900

    Style: Home 버튼 코드 스타일 변경

--- src/scenes/component/HomeButton.js
+++ src/scenes/component/HomeButton.js
@@ -1,13 +1,12 @@
-import Phaser from "phaser";
-
 export default class HomeButton{
+    
     constructor(scene, bgm){
         this.scene = scene;
         this.bgm = bgm
     }
+
     loadHomeButton(){
-        if(this.scene){
-        //home 이동 버튼
+        if(this.scene){   
         const homeButton = this.scene.add.text(
             this.scene.game.config.width - 100,
             50,
@@ -16,16 +15,11 @@ export default class HomeButton{
 
             );
             homeButton.setInteractive().on('pointerdown', ()=>{
-                //실행되고 있는 scene이 모두 종료되어야함
-                // this.scene.bgm.stop();
-                
                 this.bgm.stop();  
                 this.scene.scene.start('main');
             });
-    }else{
-        console.log("HomeButton error")
-    }
-        
-        
+        }else{
+            console.log("HomeButton error")
+        }   
     }
 }
